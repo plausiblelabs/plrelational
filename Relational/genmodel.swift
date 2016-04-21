bdcceca44d395c4a2809f709c5ea9ca6f7376a37
Mike Ash <mikeash@plausible.coop>
2016-04-21 12:39:55 -0400

    Make genmodel use a template file to make it easier to play with the generated code.

--- Relational/genmodel.swift
+++ Relational/genmodel.swift
@@ -12,30 +12,86 @@ struct Model {
     var fields: [Field]
 }
 
+enum TemplateLineComponent {
+    case Text(String)
+    case ModelName
+    case FieldName
+    case FieldType
+}
+
+struct TemplateLine {
+    var components: [TemplateLineComponent]
+    
+    var containsFieldComponent: Bool {
+        return components.contains({
+            switch $0 {
+            case .FieldName, .FieldType: return true
+            default: return false
+            }
+        })
+    }
+    
+    func render(modelName modelName: String, fieldName: String? = nil, fieldType: String? = nil) -> String {
+        let textComponents = components.map({ component -> String in
+            switch component {
+            case .Text(let text):
+                return text
+            case .ModelName:
+                return modelName
+            case .FieldName:
+                return fieldName!
+            case .FieldType:
+                return fieldType!
+            }
+        })
+        
+        return textComponents.joinWithSeparator("")
+    }
+}
+
 func trim(string: String) -> String {
     return string.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
 }
 
-func ReadModels(path: String) -> [Model] {
-    let file = fopen(path, "r")
-    defer { fclose(file) }
+class ValueWithDestructor<T> {
+    var value: T
+    let destructor: T -> Void
     
+    init(value: T, destructor: T -> Void) {
+        self.value = value
+        self.destructor = destructor
+    }
+    
+    deinit {
+        destructor(value)
+    }
+}
+
+func ReadFileByLine(path: String) -> AnyGenerator<String> {
+    let file = ValueWithDestructor(value: fopen(path, "r"), destructor: { fclose($0) })
+    precondition(file.value != nil, "Failed to open file at \(path)")
+    
+    let lineBuffer = ValueWithDestructor<UnsafeMutablePointer<Int8>>(value: nil, destructor: { free($0) })
+    var capacity = 0
+    
+    return AnyGenerator(body: {
+        let chars = getline(&lineBuffer.value, &capacity, file.value)
+        if chars == -1 { return nil }
+        
+        return String(format: "%.*s", chars, lineBuffer.value)
+    })
+}
+
+func ReadModels(path: String) -> [Model] {
     var results: [Model] = []
     
-    var buffer: UnsafeMutablePointer<Int8> = nil
-    var capacity = 0
-    var lineNumber = 0
-    while true {
+    for (index, line) in ReadFileByLine(path).enumerate() {
         func error(message: String) {
+            let lineNumber = index + 1
             fputs("\(path):\(lineNumber): error: \(message)", stderr)
             exit(1)
         }
         
-        lineNumber += 1
-        let chars = getline(&buffer, &capacity, file)
-        if chars == -1 { break }
-        
-        let line = String(format: "%.*s", chars, buffer)
         let trimmedLine = trim(line)
         guard !trimmedLine.isEmpty else { continue }
         
@@ -74,49 +130,82 @@ func ReadModels(path: String) -> [Model] {
     return results
 }
 
-func WriteModels(models: [Model], _ path: String) {
+func ReadTemplate(path: String) -> [TemplateLine] {
+    let specialTokens = [
+        "MODELNAME": TemplateLineComponent.ModelName,
+        "FIELDNAME": TemplateLineComponent.FieldName,
+        "FIELDTYPE": TemplateLineComponent.FieldType,
+    ]
+    
+    let specialTokensRegex = specialTokens.keys.map(NSRegularExpression.escapedPatternForString).joinWithSeparator("|")
+    let regex = try! NSRegularExpression(pattern: specialTokensRegex, options: [])
+    
+    var result: [TemplateLine] = []
+    
+    for line in ReadFileByLine(path) {
+        let nsline = line as NSString
+        let matches = regex.matchesInString(line, options: [], range: NSRange(location: 0, length: nsline.length))
+        let ranges = matches.map({ $0.range })
+        
+        let rangesWithDummyBeginning = [NSRange(location: 0, length: 0)] + ranges
+        let rangesWithDummyEnd = ranges + [NSRange(location: nsline.length, length: 0)]
+        
+        var line: [TemplateLineComponent] = []
+        
+        for (first, second) in zip(rangesWithDummyBeginning, rangesWithDummyEnd) {
+            if first.length != 0 {
+                let specialToken = nsline.substringWithRange(first)
+                line.append(specialTokens[specialToken]!)
+            }
+            
+            let between = NSRange(location: NSMaxRange(first), length: second.location - NSMaxRange(first))
+            if between.length != 0 {
+                let text = nsline.substringWithRange(between)
+                line.append(.Text(text))
+            }
+        }
+        
+        result.append(TemplateLine(components: line))
+    }
+    
+    return result
+}
+
+func WriteModels(models: [Model], _ template: [TemplateLine], _ path: String) {
     let file = fopen(path, "w")
     defer { fclose(file) }
     
-    func putline(line: String) {
-        fputs(line, file)
-        fputs("\n", file)
-    }
     for model in models {
-        putline("struct \(model.name) {")
-        for field in model.fields {
-            putline("    var \(field.name): \(field.type)")
+        for line in template {
+            if line.containsFieldComponent {
+                for field in model.fields {
+                    let output = line.render(modelName: model.name, fieldName: field.name, fieldType: field.type)
+                    fputs(output, file)
+                }
+            } else {
+                let output = line.render(modelName: model.name)
+                fputs(output, file)
+            }
         }
-        putline("    static func fromRow(row: Row) -> \(model.name) {")
-        putline("        return \(model.name)(")
-        for (index, field) in model.fields.enumerate() {
-            let comma = index < model.fields.count - 1 ? "," : ""
-            putline("            \(field.name): ConvertTo\(field.type)(row[\"\(field.name)\"])\(comma)")
-        }
-        putline("        )")
-        putline("    }")
-        putline("")
-        putline("    func toRow() -> Row {")
-        putline("        return Row(values: [")
-        for field in model.fields {
-            putline("            \"\(field.name)\": ConvertFrom\(field.type)(\(field.name)),")
-        }
-        putline("        ])")
-        putline("    }")
-        putline("}")
-        putline("")
     }
 }
 
-func Parse(inpath: String, _ outpath: String) {
+func Parse(model inpath: String, template: String, out outpath: String) {
     let models = ReadModels(inpath)
-    WriteModels(models, outpath)
+    let template = ReadTemplate(template)
+    WriteModels(models, template, outpath)
 }
 
-let args = Process.arguments
-
-if args.count != 3 {
-    fputs("usage: \(args[0]) <input> <output>", stderr)
+func main() {
+    let args = Process.arguments
+    
+    if args.count != 3 {
+        fputs("usage: \(args[0]) <input> <output>", stderr)
+    }
+    
+    let templatePath = ((args[0] as NSString).stringByDeletingLastPathComponent as NSString).stringByAppendingPathComponent("genmodel.template")
+    
+    Parse(model: args[1], template: templatePath, out: args[2])
 }
 
-Parse(args[1], args[2])
+main()
