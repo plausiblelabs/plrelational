b0a9a58122c7ff4cb7d88cc7aa5208cc09b62180
Mike Ash <mike@mikeash.com>
2016-04-29 09:39:06 -0400

    Add tests for Relation union, intersection, and difference.

--- RelationalTests/RelationalTests.swift
+++ RelationalTests/RelationalTests.swift
@@ -2,6 +2,21 @@
 import XCTest
 import libRelational
 
+func AssertEqual(a: Relation, _ b: Relation, file: StaticString = #file, line: UInt = #line) {
+    let aRows = mapOk(a.rows(), { $0 })
+    let bRows = mapOk(b.rows(), { $0 })
+    
+    switch (aRows, bRows) {
+    case (.Ok(let aRows), .Ok(let bRows)):
+        let aSet = Set(aRows)
+        let bSet = Set(bRows)
+        XCTAssertEqual(aSet, bSet, "Relations are not equal but should be. First relation:\n\(a)\n\nSecond relation:\n\(b)", file: file, line: line)
+    default:
+        XCTAssertNil(aRows.err)
+        XCTAssertNil(bRows.err)
+    }
+}
+
 class RelationalTests: XCTestCase {
     var dbPaths: [String] = []
     
@@ -33,5 +48,64 @@ class RelationalTests: XCTestCase {
         
         let store2 = db.fetchAll(Store.self).generate().next()!.ok!
         XCTAssertEqual(store2.name, "Joe's")
+    }
+    
+    func testUnion() {
+        let r = MakeRelation(["A",  "B",  "C"],
+                             ["A1", "B1", "C1"],
+                             ["A1", "B2", "C1"],
+                             ["A2", "B1", "C2"]
+        )
+        let s = MakeRelation(["A",  "B",  "C"],
+                             ["A1", "B2", "C1"],
+                             ["A2", "B2", "C1"],
+                             ["A2", "B2", "C2"]
+        )
+        
+        AssertEqual(r.union(s),
+                    MakeRelation(
+                        ["A",  "B",  "C"],
+                        ["A1", "B1", "C1"],
+                        ["A1", "B2", "C1"],
+                        ["A2", "B2", "C1"],
+                        ["A2", "B1", "C2"],
+                        ["A2", "B2", "C2"]))
+    }
+    
+    func testIntersection() {
+        let r = MakeRelation(["A",  "B",  "C"],
+                             ["A1", "B1", "C1"],
+                             ["A1", "B2", "C1"],
+                             ["A2", "B1", "C2"]
+        )
+        let s = MakeRelation(["A",  "B",  "C"],
+                             ["A1", "B2", "C1"],
+                             ["A2", "B2", "C1"],
+                             ["A2", "B2", "C2"]
+        )
+        
+        AssertEqual(r.intersection(s),
+                    MakeRelation(
+                        ["A",  "B",  "C"],
+                        ["A1", "B2", "C1"]))
+    }
+    
+    func testDifference() {
+        let r = MakeRelation(["A",  "B",  "C"],
+                             ["A1", "B1", "C1"],
+                             ["A1", "B2", "C1"],
+                             ["A2", "B1", "C2"]
+        )
+        let s = MakeRelation(["A",  "B",  "C"],
+                             ["A1", "B2", "C1"],
+                             ["A2", "B2", "C1"],
+                             ["A2", "B2", "C2"]
+        )
+        
+        AssertEqual(r.difference(s),
+                    MakeRelation(
+                        ["A",  "B",  "C"],
+                        ["A1", "B1", "C1"],
+                        ["A2", "B1", "C2"]))
     }
 }
