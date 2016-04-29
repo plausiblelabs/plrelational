ee4150b0f407ca70d0690023b61e03a298627bac
Mike Ash <mike@mikeash.com>
2016-04-29 09:43:53 -0400

    Test scheme equality when testing Relation equality.

--- RelationalTests/RelationalTests.swift
+++ RelationalTests/RelationalTests.swift
@@ -3,6 +3,7 @@ import XCTest
 import libRelational
 
 func AssertEqual(a: Relation, _ b: Relation, file: StaticString = #file, line: UInt = #line) {
+    XCTAssertEqual(a.scheme, b.scheme, "Relation schemes are not equal", file: file, line: line)
     let aRows = mapOk(a.rows(), { $0 })
     let bRows = mapOk(b.rows(), { $0 })
     
