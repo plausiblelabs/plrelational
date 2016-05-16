8bdf8fd54b01e7f684c319344ce54ecdb74d41f1
Mike Ash <mikeash@plausible.coop>
2016-05-16 16:01:28 -0400

    Add a debug dump func to Relation to make it easier to see how the various operation relations get nested.

---
+++ Relational/RelationDump.swift
@@ -0,0 +1,30 @@
+
+import Foundation
+
+extension Relation {
+    public func fullDebugDump(showContents showContents: Bool = true) {
+        fullDebugDump(showContents, 0)
+    }
+    
+    func fullDebugDump(showContents: Bool, _ indent: Int) {
+        let indentString = "".stringByPaddingToLength(indent * 4, withString: " ", startingAtIndex: 0)
+        func print(str: String) {
+            for line in str.componentsSeparatedByString("\n") {
+                Swift.print("\(indentString)\(line)")
+            }
+        }
+        
+        print("\(self.dynamicType)")
+        if showContents {
+            print("\(self.description)")
+        }
+        
+        let m = Mirror(reflecting: self)
+        for (name, value) in m.children {
+            if let name = name, let subrelation = value as? Relation {
+                print("\(name):")
+                subrelation.fullDebugDump(showContents, indent + 1)
+            }
+        }
+    }
+}
