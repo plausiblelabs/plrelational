7f73470b962b540ff3719b32ff553dc4f52196c8
Mike Ash <mikeash@plausible.coop>
2016-05-19 14:36:37 -0400

    Create a TransactionalDatabase which allows in-place transactional mutations.

---
+++ Relational/TransactionalDatabase.swift
@@ -0,0 +1,126 @@
+
+public class TransactionalDatabase {
+    let changeLoggingDatabase: ChangeLoggingDatabase
+    
+    var inTransaction = false
+    
+    var relations: [String: TransactionalRelation] = [:]
+    
+    public init(_ db: ChangeLoggingDatabase) {
+        self.changeLoggingDatabase = db
+    }
+    
+    public convenience init(_ db: SQLiteDatabase) {
+        self.init(ChangeLoggingDatabase(db))
+    }
+    
+    public subscript(name: String) -> TransactionalRelation {
+        if let r = relations[name] {
+            return r
+        } else {
+            let r = TransactionalRelation(underlyingRelation: changeLoggingDatabase[name])
+            if inTransaction {
+                self.beginTransactionForRelation(r)
+            }
+            relations[name] = r
+            return r
+        }
+    }
+    
+    public func beginTransaction() {
+        precondition(!inTransaction, "We don't do nested transactions (yet?)")
+        
+        for (_, r) in relations {
+            self.beginTransactionForRelation(r)
+        }
+        
+        inTransaction = true
+    }
+    
+    func beginTransactionForRelation(r: TransactionalRelation) {
+        let transactionRelation = ChangeLoggingRelation(underlyingRelation: r.underlyingRelation.underlyingRelation)
+        transactionRelation.log = r.underlyingRelation.log
+        r.transactionRelation = transactionRelation
+    }
+    
+    public func endTransaction() {
+        precondition(inTransaction, "Can't end transaction when we're not in one")
+        
+        var changes: [(TransactionalRelation, RelationChange)] = []
+        for (_, r) in relations {
+            let change = endTransactionForRelation(r)
+            changes.append((r, change))
+        }
+        
+        for (r, change) in changes {
+            r.notifyChangeObservers(change)
+        }
+        
+        inTransaction = false
+    }
+    
+    func endTransactionForRelation(r: TransactionalRelation) -> RelationChange {
+        // In computing the change log, we're assuming that target hasn't been changed.
+        // Right now we don't support directly changing the database during a transaction.
+        // If we ever do, it would involve retrying the transaction so this should still hold.
+        let underlying = r.underlyingRelation
+        let transaction = r.transactionRelation!
+        
+        let newLog = transaction.log.suffixFrom(underlying.log.count)
+        let change = underlying.dynamicType.computeChangeFromLog(newLog, underlyingRelation: underlying.computeFinalRelation().ok! /* TODO: error handling */)
+        underlying.log = transaction.log
+        return change
+    }
+    
+    public func takeSnapshot() -> ChangeLoggingDatabaseSnapshot {
+        return changeLoggingDatabase.takeSnapshot()
+    }
+    
+    public func restoreSnapshot(snapshot: ChangeLoggingDatabaseSnapshot) {
+        precondition(!inTransaction, "Can't restore a snapshot while in a transaction")
+        changeLoggingDatabase.restoreSnapshot(snapshot)
+    }
+}
+
+extension TransactionalDatabase {
+    public class TransactionalRelation: Relation, RelationDefaultChangeObserverImplementation {
+        var underlyingRelation: ChangeLoggingRelation<SQLiteTableRelation>
+        var transactionRelation: ChangeLoggingRelation<SQLiteTableRelation>?
+        
+        public var changeObserverData = RelationDefaultChangeObserverImplementationData()
+        
+        init(underlyingRelation: ChangeLoggingRelation<SQLiteTableRelation>) {
+            self.underlyingRelation = underlyingRelation
+            underlyingRelation.addWeakChangeObserver(self, method: self.dynamicType.observeUnderlyingChange)
+        }
+        
+        public var scheme: Scheme {
+            return underlyingRelation.scheme
+        }
+        
+        public func rows() -> AnyGenerator<Result<Row, RelationError>> {
+            return (transactionRelation ?? underlyingRelation).rows()
+        }
+        
+        public func contains(row: Row) -> Result<Bool, RelationError> {
+            return (transactionRelation ?? underlyingRelation).contains(row)
+        }
+        
+        public func add(row: Row) {
+            (transactionRelation ?? underlyingRelation).add(row)
+        }
+        
+        public func delete(query: SelectExpression) -> Result<Void, RelationError> {
+            return (transactionRelation ?? underlyingRelation).delete(query)
+        }
+        
+        public func update(query: SelectExpression, newValues: Row) -> Result<Void, RelationError> {
+            return (transactionRelation ?? underlyingRelation).update(query, newValues: newValues)
+        }
+        
+        func observeUnderlyingChange(change: RelationChange) {
+            self.notifyChangeObservers(change)
+        }
+    }
+}
+
---
+++ Relational/TransactionalDatabaseTests.swift
@@ -0,0 +1,228 @@
+
+import XCTest
+import libRelational
+
+class TransactionalDatabaseTests: DBTestCase {
+    func testTransactionNotifications() {
+        let sqliteDB = makeDB().db.sqliteDatabase
+        let db = TransactionalDatabase(sqliteDB)
+        let flightsScheme: Scheme = ["number", "pilot", "equipment"]
+        let pilotsScheme: Scheme = ["name", "home"]
+        
+        sqliteDB.createRelation("flights", scheme: flightsScheme)
+        sqliteDB.createRelation("pilots", scheme: pilotsScheme)
+        
+        let flights = db["flights"]
+        let pilots = db["pilots"]
+        
+        var lastFlightsChange: RelationChange?
+        _ = flights.addChangeObserver({ lastFlightsChange = $0 })
+        
+        var lastPilotsChange: RelationChange?
+        _ = pilots.addChangeObserver({ lastPilotsChange = $0 })
+        
+        db.beginTransaction()
+        
+        flights.add(["number": 1, "pilot": "Jones", "equipment": "777"])
+        flights.add(["number": 2, "pilot": "Smith", "equipment": "787"])
+        flights.add(["number": 3, "pilot": "Johnson", "equipment": "797"])
+        
+        pilots.add(["name": "Jones", "home": "New York"])
+        pilots.add(["name": "Smith", "home": "Chicago"])
+        pilots.add(["name": "Johnson", "home": "Seattle"])
+        
+        db.endTransaction()
+        
+        AssertEqual(lastFlightsChange!.added!,
+                    MakeRelation(
+                        ["number", "pilot", "equipment"],
+                        [1, "Jones", "777"],
+                        [2, "Smith", "787"],
+                        [3, "Johnson", "797"]))
+        XCTAssertTrue(lastFlightsChange!.removed!.isEmpty.ok!)
+        AssertEqual(lastPilotsChange!.added!,
+                    MakeRelation(
+                        ["name", "home"],
+                        ["Jones", "New York"],
+                        ["Smith", "Chicago"],
+                        ["Johnson", "Seattle"]))
+        XCTAssertTrue(lastPilotsChange!.removed!.isEmpty.ok!)
+        
+        db.beginTransaction()
+        
+        flights.add(["number": 4, "pilot": "Jones", "equipment": "DC-10"])
+        flights.update(Attribute("number") *== RelationValue(1 as Int64), newValues: ["pilot": "Smith"])
+        flights.delete(Attribute("equipment") *== "797")
+        
+        pilots.add(["name": "Horton", "home": "Miami"])
+        pilots.update(Attribute("name") *== "Jones", newValues: ["home": "Boston"])
+        pilots.delete(Attribute("home") *== "Seattle")
+        
+        db.endTransaction()
+    
+        AssertEqual(lastFlightsChange!.added!,
+                    MakeRelation(
+                        ["number", "pilot", "equipment"],
+                        [4, "Jones", "DC-10"],
+                        [1, "Smith", "777"]))
+        AssertEqual(lastFlightsChange!.removed!,
+                    MakeRelation(
+                        ["number", "pilot", "equipment"],
+                        [3, "Johnson", "797"],
+                        [1, "Jones", "777"]))
+        AssertEqual(lastPilotsChange!.added!,
+                    MakeRelation(
+                        ["name", "home"],
+                        ["Horton", "Miami"],
+                        ["Jones", "Boston"]))
+        AssertEqual(lastPilotsChange!.removed!,
+                    MakeRelation(
+                        ["name", "home"],
+                        ["Jones", "New York"],
+                        ["Johnson", "Seattle"]))
+    }
+    
+    func testAddNotify() {
+        let sqliteDB = makeDB().db.sqliteDatabase
+        let db = TransactionalDatabase(sqliteDB)
+        
+        sqliteDB.createRelation("flights", scheme: ["number", "pilot", "equipment"])
+        for row in MakeRelation(
+            ["number", "pilot", "equipment"],
+            ["123",    "Jones", "707"],
+            ["124",    "Steve", "727"],
+            ["125",    "Martha", "747"],
+            ["126",    "Alice", "767"],
+            ["127",    "Wendy", "707"]
+            ).rows() {
+                sqliteDB["flights"]!.add(row.ok!)
+        }
+        
+        let flights = db["flights"]
+        
+        var lastChange: RelationChange?
+        _ = flights.addChangeObserver({ lastChange = $0 })
+        
+        flights.add(["number": "42", "pilot": "Adams", "equipment": "MD-11"])
+        AssertEqual(lastChange!.added!,
+                    MakeRelation(
+                        ["number", "pilot", "equipment"],
+                        ["42",     "Adams", "MD-11"]))
+        XCTAssertNil(lastChange!.removed)
+        
+        flights.add(["number": "43", "pilot": "Adams", "equipment": "MD-11"])
+        AssertEqual(lastChange!.added!,
+                    MakeRelation(
+                        ["number", "pilot", "equipment"],
+                        ["43",     "Adams", "MD-11"]))
+        XCTAssertNil(lastChange!.removed)
+        
+        flights.add(["number": "44", "pilot": "Adams", "equipment": "MD-11"])
+        AssertEqual(lastChange!.added!,
+                    MakeRelation(
+                        ["number", "pilot", "equipment"],
+                        ["44",     "Adams", "MD-11"]))
+        XCTAssertNil(lastChange!.removed)
+        
+        flights.add(["number": "45", "pilot": "Adams", "equipment": "MD-11"])
+        AssertEqual(lastChange!.added!,
+                    MakeRelation(
+                        ["number", "pilot", "equipment"],
+                        ["45",     "Adams", "MD-11"]))
+        XCTAssertNil(lastChange!.removed)
+    }
+    
+    func testDeleteNotify() {
+        let sqliteDB = makeDB().db.sqliteDatabase
+        let db = TransactionalDatabase(sqliteDB)
+        
+        sqliteDB.createRelation("flights", scheme: ["number", "pilot", "equipment"])
+        for row in MakeRelation(
+            ["number", "pilot", "equipment"],
+            ["123",    "Jones", "707"],
+            ["124",    "Steve", "727"],
+            ["125",    "Martha", "747"],
+            ["126",    "Alice", "767"],
+            ["127",    "Wendy", "707"]
+            ).rows() {
+                sqliteDB["flights"]!.add(row.ok!)
+        }
+        
+        let flights = db["flights"]
+        
+        var lastChange: RelationChange?
+        _ = flights.addChangeObserver({ lastChange = $0 })
+        
+        flights.add(["number": "42", "pilot": "Adams", "equipment": "MD-11"])
+        
+        flights.delete(Attribute("number") *== "42")
+        AssertEqual(lastChange!.removed!,
+                    MakeRelation(
+                        ["number", "pilot", "equipment"],
+                        ["42",     "Adams", "MD-11"]))
+        XCTAssertNil(lastChange!.added)
+        
+        
+        flights.delete(Attribute("number") *== "123")
+        AssertEqual(lastChange!.removed!,
+                    MakeRelation(
+                        ["number", "pilot", "equipment"],
+                        ["123",    "Jones", "707"]))
+        XCTAssertNil(lastChange!.added)
+    }
+    
+    func testUpdateNotify() {
+        let sqliteDB = makeDB().db.sqliteDatabase
+        let db = TransactionalDatabase(sqliteDB)
+        
+        sqliteDB.createRelation("flights", scheme: ["number", "pilot", "equipment"])
+        for row in MakeRelation(
+            ["number", "pilot", "equipment"],
+            ["123",    "Jones", "707"],
+            ["124",    "Steve", "727"],
+            ["125",    "Martha", "747"],
+            ["126",    "Alice", "767"],
+            ["127",    "Wendy", "707"]
+            ).rows() {
+                sqliteDB["flights"]!.add(row.ok!)
+        }
+        
+        let flights = db["flights"]
+        
+        var lastChange: RelationChange?
+        _ = flights.addChangeObserver({ lastChange = $0 })
+        
+        flights.add(["number": "42", "pilot": "Adams", "equipment": "MD-11"])
+        flights.update(Attribute("number") *== "42", newValues: ["equipment": "DC-10"])
+        AssertEqual(lastChange!.removed!,
+                    MakeRelation(
+                        ["number", "pilot", "equipment"],
+                        ["42",     "Adams", "MD-11"]))
+        AssertEqual(lastChange!.added!,
+                    MakeRelation(
+                        ["number", "pilot", "equipment"],
+                        ["42",     "Adams", "DC-10"]))
+        
+        flights.update(Attribute("number") *== "123", newValues: ["equipment": "DC-10"])
+        AssertEqual(lastChange!.removed!,
+                    MakeRelation(
+                        ["number", "pilot", "equipment"],
+                        ["123",    "Jones", "707"]))
+        AssertEqual(lastChange!.added!,
+                    MakeRelation(
+                        ["number", "pilot", "equipment"],
+                        ["123",    "Jones", "DC-10"]))
+        
+        flights.update(Attribute("equipment") *== "DC-10", newValues: ["pilot": "JFK"])
+        AssertEqual(lastChange!.removed!,
+                    MakeRelation(
+                        ["number", "pilot", "equipment"],
+                        ["42",     "Adams", "DC-10"],
+                        ["123",    "Jones", "DC-10"]))
+        AssertEqual(lastChange!.added!,
+                    MakeRelation(
+                        ["number", "pilot", "equipment"],
+                        ["42",     "JFK", "DC-10"],
+                        ["123",    "JFK", "DC-10"]))
+    }
+}
