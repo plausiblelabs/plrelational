ac8bab8c4bf01533448caeff113a4096c6924ad3
Chris Campbell <campbell@plausible.coop>
2016-07-12 13:14:07 -0700

    Sketch out some async-aware property types.
    These are similar to (and co-exist with) the existing property types, except that `value` is now optional and we don't
    bother with the `valueChanging` optimizations.

---
+++ BindingTests/AsyncPropertyTests.swift
@@ -0,0 +1,8 @@
+//
+// Copyright (c) 2016 Plausible Labs Cooperative, Inc.
+// All rights reserved.
+//
+
+import XCTest
+@testable import Binding
+
---
+++ BindingTests/RelationAsyncPropertyTests.swift
@@ -0,0 +1,15 @@
+//
+// Copyright (c) 2016 Plausible Labs Cooperative, Inc.
+// All rights reserved.
+//
+
+import XCTest
+import libRelational
+@testable import Binding
+
+class RelationAsyncPropertyTests: BindingTestCase {
+    
+    func testReadOnlyProperty() {
+        // TODO
+    }
+}
