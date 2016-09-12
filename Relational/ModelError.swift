d73a9676cc43378b740c4b0c54340a3bd951bfe0
Mike Ash <mikeash@plausible.coop>
2016-09-12 13:03:58 -0400

    Delete Model and related code, as we settled on another approach and that stuff is just going unused, and is interfering with migration.

--- Relational/ModelError.swift
+++
@@ -1,8 +0,0 @@
-//
-// Copyright (c) 2016 Plausible Labs Cooperative, Inc.
-// All rights reserved.
-//
-
-public enum ModelError: Error {
-    case badDataType(attribute: String)
-}
--- Relational/WeakReference.swift
+++
@@ -1,8 +0,0 @@
-//
-// Copyright (c) 2016 Plausible Labs Cooperative, Inc.
-// All rights reserved.
-//
-
-struct WeakReference<T: AnyObject> {
-    weak var value: T?
-}
