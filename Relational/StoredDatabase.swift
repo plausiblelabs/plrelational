42c06e4dcfaa5621f302e62027f8e7c9b286afb0
Chris Campbell <campbell@plausible.coop>
2016-09-20 12:29:11 -0700

    Drop unused SQLiteBinding class and stub out StoredDatabase/Relation protocols.
    Issue: VP-187

---
+++ Relational/StoredDatabase.swift
@@ -0,0 +1,10 @@
+//
+// Copyright (c) 2016 Plausible Labs Cooperative, Inc.
+// All rights reserved.
+//
+
+import Foundation
+
+protocol StoredDatabase {
+    associatedtype Relation: StoredRelation
+}
