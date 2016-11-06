22a474e7fc6e1eeb6bca648debc1d9160c8026dc
Chris Campbell <campbell@plausible.coop>
2016-11-06 15:17:27 -0800

    Show some placeholder tables in the editor view when the selection changes.

--- Examples/Visualizer/EditorView.swift
+++ Examples/Visualizer/EditorView.swift
@@ -27,70 +27,102 @@ class EditorView: BackgroundView {
     
     private var observerRemovals: [ObserverRemoval] = []
     
-//    private var relTableView1: RelationTableView!
-//    private var relTableView2: RelationTableView!
-//    private var relTableView3: RelationTableView!
-    
     init(frame: NSRect, model: DocModel) {
         self.docModel = model
         
         super.init(frame: frame)
         
-//        let employees = MakeRelation(
-//            ["emp_id", "emp_name", "dept_name"],
-//            [1, "Alice", "Sales"],
-//            [2, "Bob", "Finance"],
-//            [3, "Carlos", "Production"],
-//            [4, "Donald", "Production"])
-//        
-//        let departments = MakeRelation(
-//            ["dept_name", "manager_id"],
-//            ["Sales", 1],
-//            ["Production", 3])
-//        
-//        let joined = employees.leftOuterJoin(departments)
-//        
-//        func tableView(relation: Relation,
-//                       idAttr: Attribute, orderedAttrs: [Attribute],
-//                       underlying: NSTableView) -> RelationTableView
-//        {
-//            let columns = orderedAttrs.map{ RelationTableColumnModel(identifier: $0, title: $0.name) }
-//            let data = relation.arrayProperty(idAttr: idAttr, orderAttr: idAttr)
-//            let model = TableViewModel(
-//                columns: columns,
-//                data: data,
-//                cellText: { attribute, row in
-//                    let rowID = row[idAttr]
-//                    // TODO: For now we will convert non-string values to a string for display in
-//                    // the cell, but eventually we should have native support for these
-//                    let initialStringValue = row[attribute].description
-//                    let textProperty = relation
-//                        .select(idAttr *== rowID)
-//                        .project(attribute)
-//                        .asyncProperty(initialValue: initialStringValue, { $0.oneValueOrNil($1)?.description ?? "" })
-//                    return .asyncReadOnly(textProperty)
-//            }
-//            )
-//            let view = TableView(model: model, tableView: underlying)
-//            view.animateChanges = true
-//            return view
-//        }
-//        
-//        relTableView1 = tableView(
-//            relation: employees,
-//            idAttr: "emp_id",
-//            orderedAttrs: ["emp_id", "emp_name", "dept_name"],
-//            underlying: tableView1)
-//        relTableView2 = tableView(
-//            relation: departments,
-//            idAttr: "dept_name",
-//            orderedAttrs: ["dept_name", "manager_id"],
-//            underlying: tableView2)
-//        relTableView3 = tableView(
-//            relation: joined,
-//            idAttr: "emp_id",
-//            orderedAttrs: ["emp_id", "emp_name", "dept_name", "manager_id"],
-//            underlying: tableView3)
+        let activeItemRemoval = model.activeTabCurrentHistoryItem.signal.observe{ historyItem, _ in
+            if let historyItem = historyItem {
+                let docOutlinePath = historyItem.outlinePath
+                switch docOutlinePath.type {
+                case .storedRelation, .sharedRelation, .privateRelation:
+                    self.setRelationContent(docOutlinePath)
+                default:
+                    self.contentView?.removeFromSuperview()
+                }
+            } else {
+                self.contentView?.removeFromSuperview()
+            }
+        }
+        observerRemovals.append(activeItemRemoval)
+        model.activeTabCurrentHistoryItem.start()
+    }
+    
+    private func prepareForNewContent() {
+        // TODO: Commit changes for current selection
+        contentView?.removeFromSuperview()
+    }
+    
+    func setRelationContent(_ docOutlinePath: DocOutlinePath) {
+        prepareForNewContent()
+
+        let chainView = BackgroundView(frame: self.bounds)
+        
+        let employees = MakeRelation(
+            ["emp_id", "emp_name", "dept_name"],
+            [1, "Alice", "Sales"],
+            [2, "Bob", "Finance"],
+            [3, "Carlos", "Production"],
+            [4, "Donald", "Production"])
+        
+        let departments = MakeRelation(
+            ["dept_name", "manager_id"],
+            ["Sales", 1],
+            ["Production", 3])
+        
+        let joined = employees.leftOuterJoin(departments)
+
+        func addTableView(x: CGFloat, y: CGFloat, relation: Relation,
+                          idAttr: Attribute, orderedAttrs: [Attribute])
+        {
+            let columns = orderedAttrs.map{ RelationTableColumnModel(identifier: $0, title: $0.name) }
+            let data = relation.arrayProperty(idAttr: idAttr, orderAttr: idAttr)
+            let model = TableViewModel(
+                columns: columns,
+                data: data,
+                cellText: { attribute, row in
+                    let rowID = row[idAttr]
+                    // TODO: For now we will convert non-string values to a string for display in
+                    // the cell, but eventually we should have native support for these
+                    let initialStringValue = row[attribute].description
+                    let textProperty = relation
+                        .select(idAttr *== rowID)
+                        .project(attribute)
+                        .asyncProperty(initialValue: initialStringValue, { $0.oneValueOrNil($1)?.description ?? "" })
+                    return .asyncReadOnly(textProperty)
+                }
+            )
+
+            let scrollView = NSScrollView(frame: NSMakeRect(x, y, 340, 200))
+            let nsTableView = NSTableView(frame: scrollView.bounds)
+            nsTableView.allowsColumnResizing = true
+            nsTableView.allowsColumnReordering = false
+            scrollView.documentView = nsTableView
+            scrollView.hasVerticalScroller = true
+            let view = TableView(model: model, tableView: nsTableView)
+            view.animateChanges = true
+            chainView.addSubview(scrollView)
+        }
+
+        addTableView(
+            x: 20, y: 20,
+            relation: employees,
+            idAttr: "emp_id",
+            orderedAttrs: ["emp_id", "emp_name", "dept_name"])
+        addTableView(
+            x: 400, y: 20,
+            relation: departments,
+            idAttr: "dept_name",
+            orderedAttrs: ["dept_name", "manager_id"])
+        addTableView(
+            x: 20, y: 260,
+            relation: joined,
+            idAttr: "emp_id",
+            orderedAttrs: ["emp_id", "emp_name", "dept_name", "manager_id"])
+        
+        addSubview(chainView)
+        self.contentView = chainView
     }
     
     required init?(coder: NSCoder) {
