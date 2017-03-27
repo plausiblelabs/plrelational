2715109361841b54fb096d3accb1ece30137f19e
Chris Campbell <campbell@plausible.coop>
2017-03-27 12:23:12 -0700

    Fix module path for macOS and iOS targets.
    The macOS and iOS framework targets are now building.  The macOS tests are passing, but iOS tests are failing.
    
    Issue: VP-228

--- PLRelational/PlaygroundMonospace.swift
+++ PLRelational/PlaygroundMonospace.swift
@@ -3,14 +3,22 @@
 // All rights reserved.
 //
 
-import Cocoa
+#if os(macOS)
+import AppKit
+#else
+import UIKit
+#endif
 
 public protocol PlaygroundMonospace: CustomStringConvertible, CustomPlaygroundQuickLookable {}
 
 extension PlaygroundMonospace {
     public var customPlaygroundQuickLook: PlaygroundQuickLook {
+#if os(macOS)
         let attrstr = NSAttributedString(string: self.description,
                                          attributes: [NSFontAttributeName: NSFont(name: "Monaco", size: 9)!])
+#else
+        let attrstr = NSAttributedString(string: self.description)
+#endif
         return PlaygroundQuickLook(reflecting: attrstr)
     }
 }
