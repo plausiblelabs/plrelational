d26993be0d942c0f492e747ce4ec9931e0460892
Mike Ash <mike@mikeash.com>
2017-03-27 20:24:05 -0400

    Fix ObjectMap not to generate negative indexes when the top bit of an object address is set.

--- PLRelational/ObjectMap.swift
+++ PLRelational/ObjectMap.swift
@@ -53,7 +53,9 @@ class ObjectMap<Value> {
         // Objects are 16-byte aligned, so shift off the last four bits to get a better hash value.
         // If this ends up being wrong this code will still work, but hash collisions will be more
         // frequent so performance will suffer.
-        let hash = key >> 4
+        // Perform the shift as an unsigned number, to avoid producing indexes with
+        // negative values.
+        let hash = Int(bitPattern: UInt(bitPattern: key) >> 4)
         var index = hash % capacity
         var firstDead: Int? = nil
         while table[index].key != key && table[index].key != EMPTY {
