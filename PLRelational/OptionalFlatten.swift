d0eb8a91dfeae493fb99816318c45ccd95510e50
Mike Ash <mikeash@plausible.coop>
2017-05-17 11:48:10 -0400

    Add a convenience function to flatten two optionals into an optional tuple of non-optionals. Make public the convenience function to flatten a double optional into a single optional.

--- PLRelational/OptionalFlatten.swift
+++ PLRelational/OptionalFlatten.swift
@@ -5,9 +5,19 @@
 
 
 /// Collapse a double optional into a single optional, transforming .some(nil) into nil.
-func flatten<T>(_ doubleOptional: T??) -> T? {
+public func flatten<T>(_ doubleOptional: T??) -> T? {
     switch doubleOptional {
     case .none, .some(.none): return nil
     case .some(.some(let wrapped)): return wrapped
+    }
+}
+
+/// Collapse two optionals into an optional tuple with non-optional components.
+/// If either parameter is nil, the return value is nil.
+public func flatten<T, U>(_ t: T?, _ u: U?) -> (T, U)? {
+    if let t = t, let u = u {
+        return (t, u)
+    } else {
+        return nil
     }
 }
