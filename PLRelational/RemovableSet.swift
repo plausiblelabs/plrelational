49c4e2f94ddbde9ffbee144a79198263773a1503
Mike Ash <mikeash@plausible.coop>
2017-05-23 13:50:59 -0400

    Add a simple RemovableSet class which wraps up the common observer pattern with a dictionary mapping UInt64 to functions.

---
+++ PLRelational/RemovableSet.swift
@@ -0,0 +1,29 @@
+//
+// Copyright (c) 2017 Plausible Labs Cooperative, Inc.
+// All rights reserved.
+//
+
+
+/// A set of arbitrary objects which can be iterated and can later be removed.
+/// Ideal for observer callbacks.
+public class RemovableSet<T>: Sequence {
+    private var nextNumber: UInt64 = 0
+    private var contents: [UInt64: T] = [:]
+    
+    /// Add a value to the set. Returns a remover which, when called, removes the value from the set.
+    public func add(_ value: T) -> RemovableSetRemover {
+        let number = nextNumber
+        nextNumber += 1
+        
+        contents[number] = value
+        return {
+            self.contents.removeValue(forKey: number)
+        }
+    }
+    
+    public func makeIterator() -> LazyMapIterator<DictionaryIterator<UInt64, T>, T> {
+        return contents.values.makeIterator()
+    }
+}
+
+public typealias RemovableSetRemover = (Void) -> Void
