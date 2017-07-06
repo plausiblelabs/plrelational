6c127f713ad4bc3a8d11bd42a961525f334bde3d
Chris Campbell <campbell@plausible.coop>
2017-07-06 16:51:05 -0700

    Get the BindableControlsApp building (but not quite working) again.

--- Examples/BindableControlsApp/AppDelegate.swift
+++ Examples/BindableControlsApp/AppDelegate.swift
@@ -14,15 +14,15 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate {
     @IBOutlet weak var window: NSWindow!
     @IBOutlet var rootView: BackgroundView!
     @IBOutlet var outlineView: ExtOutlineView!
-    var textField: TextField!
-    var checkbox: Checkbox!
-    var popupButton: PopUpButton<String>!
-    var stepper: StepperView!
-    var comboBox: ComboBox<String>!
-    var colorPicker: ColorPickerView!
+    private var textField: TextField!
+    private var checkbox: Checkbox!
+    private var popupButton: PopUpButton<String>!
+    private var stepper: StepperView!
+    private var comboBox: ComboBox<String>!
+    private var colorPicker: ColorPickerView!
 
-    var nsUndoManager: SPUndoManager!
-    var listView: ListView<RowArrayElement>!
+    private var nsUndoManager: SPUndoManager!
+    private var listView: ListView<RowArrayElement>!
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // By default, NSColor is set to "ignore alpha" which means that color wells
@@ -53,25 +53,25 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate {
             precondition(createResult.ok != nil)
             return db[name]
         }
-        var objects = createRelation("object", ["id", "name", "editable", "best_friend", "fav_day", "fav_color", "rocks", "parent", "order"])
-        var selectedObjectID = createRelation("selected_object", ["id"])
-        let selectedObjects = selectedObjectID.join(objects)
-        let selectedObjectsName = selectedObjects.project(["name"])
-        let selectedObjectsEditable = selectedObjects.project(["editable"])
-        let selectedObjectsFriend = selectedObjects.project(["best_friend"])
-        let selectedObjectsDay = selectedObjects.project(["fav_day"])
-        let selectedObjectsColor = selectedObjects.project(["fav_color"])
-        let selectedObjectsRocks = selectedObjects.project(["rocks"])
+        var persons = createRelation("person", ["id", "name", "friendly", "best_friend", "fav_day", "fav_color", "rocks", "parent", "order"])
+        var selectedPersonID = createRelation("selected_person", ["id"])
+        let selectedPersons = selectedPersonID.join(persons)
+        let selectedPersonsName = selectedPersons.project(["name"])
+        let selectedPersonsFriendly = selectedPersons.project(["friendly"])
+        let selectedPersonsFriend = selectedPersons.project(["best_friend"])
+        let selectedPersonsDay = selectedPersons.project(["fav_day"])
+        let selectedPersonsColor = selectedPersons.project(["fav_color"])
+        let selectedPersonsRocks = selectedPersons.project(["rocks"])
         
         // Prepare the undo manager
         nsUndoManager = SPUndoManager()
         let undoManager = UndoManager(nsmanager: nsUndoManager)
         let undoableDB = UndoableDatabase(db: db, undoManager: undoManager)
 
-        // Add some test objects
+        // Add some test persons
         var id: Int64 = 1
         var order: Double = 1.0
-        func addObject(_ name: String, editable: Bool, day: String?, color: Color?, rocks: Int64) {
+        func addPerson(_ name: String, friendly: Bool, day: String?, color: Color?, rocks: Int64) {
             let dayValue: RelationValue
             if let day = day {
                 dayValue = RelationValue(day)
@@ -89,7 +89,7 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate {
             let row: Row = [
                 "id": RelationValue(id),
                 "name": RelationValue(name),
-                "editable": RelationValue(Int64(editable ? 1 : 0)),
+                "friendly": RelationValue(Int64(friendly ? 1 : 0)),
                 "best_friend": .null,
                 "fav_day": dayValue,
                 "fav_color": colorValue,
@@ -97,20 +97,20 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate {
                 "parent": .null,
                 "order": RelationValue(order)
             ]
-            _  = objects.add(row)
+            _  = persons.add(row)
             
             id += 1
             order += 1.0
         }
-        addObject("Fred", editable: false, day: nil, color: nil, rocks: 17)
-        addObject("Wilma", editable: true, day: "Friday", color: Color.blue, rocks: 42)
+        addPerson("Fred", friendly: false, day: nil, color: nil, rocks: 17)
+        addPerson("Wilma", friendly: true, day: "Friday", color: Color.blue, rocks: 42)
 
-        func nameProperty(_ relation: Relation, initialValue: String??) -> AsyncReadWriteProperty<String?> {
+        func nameProperty(_ relation: Relation, initialValue: String?) -> AsyncReadWriteProperty<String> {
             return undoableDB.bidiProperty(
                 action: "Rename Person",
-                signal: relation.oneStringOrNil(initialValue: initialValue),
+                signal: relation.oneString(initialValue: initialValue),
                 update: {
-                    relation.asyncUpdateNullableString($0)
+                    relation.asyncUpdateString($0)
                 }
             )
         }
@@ -125,102 +125,96 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate {
         
         // Set up the list view
         let listViewModel = ListViewModel(
-            data: objects.arrayProperty(idAttr: "id", orderAttr: "order"),
+            data: persons.arrayProperty(idAttr: "id", orderAttr: "order"),
             contextMenu: nil,
             move: nil,
             cellIdentifier: { _ in "PageCell" },
             cellText: { row in
                 let rowID = row["id"]
                 let initialValue: String? = row["name"].get()
-                let nameRelation = objects.select(Attribute("id") *== rowID).project(["name"])
-                return .asyncReadWriteOpt(nameProperty(nameRelation, initialValue: initialValue))
+                let nameRelation = persons.select(Attribute("id") *== rowID).project(["name"])
+                return .asyncReadWrite(nameProperty(nameRelation, initialValue: initialValue))
             },
             cellImage: nil
         )
         listView = ListView(model: listViewModel, outlineView: outlineView)
         listView.animateChanges = true
-        listView.selection <~> listSelectionProperty(selectedObjectID)
+        listView.selection <~> listSelectionProperty(selectedPersonID)
 
         // Add some other controls (could also do this in the xib)
         textField = TextField(frame: NSMakeRect(200, 30, 200, 24))
         textField.setAccessibilityIdentifier("NameField")
         rootView.addSubview(textField)
         
-//        checkbox = Checkbox(frame: NSMakeRect(200, 80, 120, 24))
-//        checkbox.title = "Editable"
-//        rootView.addSubview(checkbox)
-//
-//        popupButton = PopUpButton(frame: NSMakeRect(200, 120, 120, 24), pullsDown: false)
-//        popupButton.setAccessibilityIdentifier("Day")
-//        rootView.addSubview(popupButton)
-//
-//        stepper = StepperView(frame: NSMakeRect(200, 160, 120, 24), min: 0, max: 999, defaultValue: 0)
-//        rootView.addSubview(stepper)
-//
-//        comboBox = ComboBox(frame: NSMakeRect(200, 200, 120, 24))
-//        rootView.addSubview(comboBox)
-//        
-//        colorPicker = ColorPickerView(defaultColor: Color.white)
-//        colorPicker.frame = NSMakeRect(200, 240, 200, 24)
-//        rootView.addSubview(colorPicker)
+        checkbox = Checkbox(frame: NSMakeRect(200, 80, 120, 24))
+        checkbox.title = "Editable"
+        rootView.addSubview(checkbox)
+
+        popupButton = PopUpButton(frame: NSMakeRect(200, 120, 120, 24), pullsDown: false)
+        popupButton.setAccessibilityIdentifier("Day")
+        rootView.addSubview(popupButton)
+
+        stepper = StepperView(frame: NSMakeRect(200, 160, 120, 24), min: 0, max: 999, defaultValue: 0)
+        rootView.addSubview(stepper)
+
+        comboBox = ComboBox(frame: NSMakeRect(200, 200, 120, 24))
+        rootView.addSubview(comboBox)
+
+        colorPicker = ColorPickerView(defaultColor: Color.white)
+        colorPicker.frame = NSMakeRect(200, 240, 240, 24)
+        rootView.addSubview(colorPicker)
         
         // Set up the bindings between controls and view model
         textField.deliverTransientChanges = true
-        _ = textField.optString <~> nameProperty(selectedObjectsName, initialValue: nil)
-        //_ = textField.placeholder <~ selectedObjectsName.stringWhenMulti("Multiple Values")
+        textField.string <~> nameProperty(selectedPersonsName, initialValue: nil)
+        //textField.placeholder <~ selectedPersonsName.stringWhenMulti("Multiple Values")
 
-//        checkbox.checked <~> undoableDB.bidiProperty(
-//            selectedObjectsEditable,
-//            action: "Change Editable",
-//            get: { CheckState($0.oneBoolOrNil) },
-//            set: { selectedObjectsEditable.updateBoolean($0.boolValue) }
-//        )
-//        
-//        let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-//        let popupItems = days.map{ titledMenuItem($0) }
-//        popupButton.items <~ constantValueProperty(popupItems)
-//        popupButton.defaultItemContent = MenuItemContent(object: "Default", title: selectedObjectsColor.stringWhenMulti("Multiple", otherwise: "Default"))
-//        popupButton.selectedObject <~> undoableDB.bidiProperty(
-//            selectedObjectsDay,
-//            action: "Change Favorite Day",
-//            get: { $0.oneStringOrNil },
-//            set: { selectedObjectsDay.updateNullableString($0) }
-//        )
-//        
-//        stepper.value <~> undoableDB.bidiProperty(
-//            selectedObjectsRocks,
-//            action: "Change Rocks",
-//            get: { $0.oneIntegerOrNil.map{ Int($0) } },
-//            set: { selectedObjectsRocks.updateInteger(Int64($0!)) }
-//        )
-//        stepper.placeholder <~ selectedObjectsRocks.stringWhenMulti("Multiple", otherwise: "Default")
-//        
-//        comboBox.items <~ constantValueProperty(["Alice", "Bob", "Carlos"])
-//        comboBox.value <~> undoableDB.bidiProperty(
-//            selectedObjectsFriend,
-//            action: "Change Best Friend",
-//            get: { $0.oneStringOrNil },
-//            set: { selectedObjectsFriend.updateNullableString($0) }
-//        )
-//        comboBox.placeholder <~ selectedObjectsFriend.stringWhenMulti("Multiple", otherwise: "Default")
-//        
-//        colorPicker.color <~> undoableDB.bidiProperty(
-//            selectedObjectsColor,
-//            action: "Change Favorite Color",
-//            get: {
-//                $0.commonValue{ rv -> Color? in
-//                    if let s: String = rv.get() {
-//                        return Color(string: s)
-//                    } else {
-//                        return nil
-//                    }
-//                }
-//            },
-//            set: { (commonValue: CommonValue<Color>) in
-//                guard let color = commonValue.orNil() else { preconditionFailure("Expected a single color value") }
-//                selectedObjectsColor.updateString(color.stringValue)
-//            }
-//        )
+        // TODO: Disable or clear when nothing selected
+        checkbox.checkState <~> undoableDB.bidiProperty(
+            action: "Change Friendly",
+            signal: selectedPersonsFriendly.oneBoolOrNil().map{ CheckState($0) },
+            update: { selectedPersonsFriendly.asyncUpdateBoolean($0.boolValue) }
+        )
+        
+        let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+        let popupItems = days.map{ titledMenuItem($0) }
+        popupButton.items <~ constantValueProperty(popupItems)
+        //popupButton.defaultItemContent = MenuItemContent(object: "Default", title: selectedPersonsColor.stringWhenMulti("Multiple", otherwise: "Default"))
+        popupButton.selectedObject <~> undoableDB.bidiProperty(
+            action: "Change Favorite Day",
+            signal: selectedPersonsDay.oneStringOrNil(),
+            update: { selectedPersonsDay.asyncUpdateNullableString($0) }
+        )
+
+        stepper.value <~> undoableDB.bidiProperty(
+            action: "Change Rocks",
+            signal: selectedPersonsRocks.oneIntegerOrNil().map{ $0.map{ Int($0) } },
+            update: { selectedPersonsRocks.asyncUpdateInteger(Int64($0!)) }
+        )
+        //stepper.placeholder <~ selectedPersonsRocks.stringWhenMulti("Multiple", otherwise: "Default")
+
+        comboBox.items <~ constantValueProperty(["Alice", "Bob", "Carlos"])
+        comboBox.value <~> undoableDB.bidiProperty(
+            action: "Change Best Friend",
+            signal: selectedPersonsFriend.oneStringOrNil(),
+            update: { selectedPersonsFriend.asyncUpdateNullableString($0) }
+        )
+        //comboBox.placeholder <~ selectedPersonsFriend.stringWhenMulti("Multiple", otherwise: "Default")
+
+        colorPicker.color <~> undoableDB.bidiProperty(
+            action: "Change Favorite Color",
+            signal: selectedPersonsColor.commonValue{ rv -> Color? in
+                if let s: String = rv.get() {
+                    return Color(string: s)
+                } else {
+                    return nil
+                }
+            },
+            update: { (commonValue: CommonValue<Color>) in
+                guard let color = commonValue.orNil() else { preconditionFailure("Expected a single color value") }
+                selectedPersonsColor.asyncUpdateString(color.stringValue)
+            }
+        )
     }
     
     func windowWillReturnUndoManager(_ window: NSWindow) -> Foundation.UndoManager? {
