15fe4a7d4e7dc5bd826d21768da911d02769a23e
Chris Campbell <campbell@plausible.coop>
2017-08-30 22:03:25 -0700

    Sketch out xibs for TodoApp.

--- Examples/TodoApp/AppDelegate.swift
+++ Examples/TodoApp/AppDelegate.swift
@@ -12,7 +12,12 @@ import PLBindableControls
 class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate {
     
     @IBOutlet weak var window: NSWindow!
-    @IBOutlet weak var tableContainer: NSView!
+    @IBOutlet weak var listContainer: NSView!
+    @IBOutlet weak var detailContainer: NSView!
+    @IBOutlet weak var noSelectionLabel: Label!
+    
+    private var checklistView: ChecklistView!
+    private var detailView: DetailView!
     
     private var model: Model!
     
@@ -21,5 +26,15 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate {
         
         // Initialize our model
         model = Model()
+        
+        // Add the checklist view to the left side
+        let checklistViewModel = ChecklistViewModel(model: model)
+        checklistView = ChecklistView(frame: listContainer.bounds, model: checklistViewModel)
+        listContainer.addSubview(checklistView)
+        
+        // Add the detail view to the left side
+        let detailViewModel = DetailViewModel(model: model)
+        detailView = DetailView(frame: detailContainer.bounds, model: detailViewModel)
+        detailContainer.addSubview(detailView)
     }
 }
--- Examples/TodoApp/ChecklistView.swift
+++ Examples/TodoApp/ChecklistView.swift
@@ -7,6 +7,25 @@ import Cocoa
 import PLRelational
 import PLRelationalBinding
 
-class ChecklistView {
+class ChecklistView: NSView {
+
+    @IBOutlet var view: NSView!
+    @IBOutlet var outlineView: NSOutlineView!
+
+    private let model: ChecklistViewModel
+
+    init(frame: NSRect, model: ChecklistViewModel) {
+        self.model = model
+        
+        super.init(frame: frame)
+        
+        Bundle.main.loadNibNamed("ChecklistView", owner: self, topLevelObjects: nil)
+        view.frame = self.bounds
+        addSubview(view)
+    }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
 }
