1308d57dc387f0da100cf5fa4d6b5ff9eeb8d35b
Chris Campbell <campbell@plausible.coop>
2017-08-31 11:58:58 -0700

    Customize the list view and row backgrounds just to make things a bit prettier.

--- Examples/TodoApp/ChecklistView.swift
+++ Examples/TodoApp/ChecklistView.swift
@@ -27,13 +27,67 @@ class ChecklistView: NSView {
         view.frame = self.bounds
         addSubview(view)
         
+        // Configure the views
+        outlineView.backgroundColor = .clear
+        
         // Bind to our view model
-        listView = ListView(model: model.itemsListModel, outlineView: outlineView)
+        listView = CustomListView(model: model.itemsListModel, outlineView: outlineView)
         listView.selection <~> model.itemsListSelection
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+}
 
+/// Normally it would not be necessary to subclass ListView, but we do that here just to customize the row backgrounds.
+private class CustomListView: ListView<RowArrayElement> {
+    
+    override func outlineView(_ outlineView: NSOutlineView, rowViewForItem item: Any) -> NSTableRowView? {
+        let identifier = "RowView"
+        if let rowView = outlineView.make(withIdentifier: identifier, owner: self) {
+            return rowView as? NSTableRowView
+        } else {
+            let rowView = OutlineRowView(frame: NSZeroRect, rowHeight: outlineView.rowHeight)
+            rowView.identifier = identifier
+            return rowView
+        }
+    }
+}
+
+private let padX: CGFloat = 1
+private let cornerRadius: CGFloat = 4
+private let strongColor: NSColor = NSColor(red: 41.0/255.0, green: 191.0/255.0, blue: 250.0/255.0, alpha: 1.0)
+private let weakColor: NSColor = NSColor(white: 0.83, alpha: 1.0)
+
+private class OutlineRowView: NSTableRowView {
+    let rowHeight: CGFloat
+    
+    init(frame: NSRect, rowHeight: CGFloat) {
+        self.rowHeight = rowHeight
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func drawBackground(in dirtyRect: NSRect) {
+        drawRounded(.white)
+    }
+    
+    override func drawSelection(in dirtyRect: NSRect) {
+        drawRounded(self.isEmphasized ? strongColor : weakColor)
+    }
+    
+    private func drawRounded(_ color: NSColor) {
+        var rect = self.bounds
+        rect.origin.x = padX
+        rect.origin.y = rect.height - rowHeight
+        rect.size.width -= padX * 2
+        rect.size.height = rowHeight
+        color.setFill()
+        let path = NSBezierPath(roundedRect: rect, xRadius: cornerRadius, yRadius: cornerRadius)
+        path.fill()
+    }
 }
