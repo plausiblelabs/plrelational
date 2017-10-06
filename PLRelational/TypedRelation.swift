483f29ecb36da400da8a6e901fbd470f1c78cb5e
Mike Ash <mikeash@plausible.coop>
2017-10-06 13:41:52 -0400

    Add more docs to TypedRelation.

--- PLRelational/TypedRelation.swift
+++ PLRelational/TypedRelation.swift
@@ -3,7 +3,9 @@
 // All rights reserved.
 //
 
+/// A `Relation` whose scheme is a single typed attribute.
 public struct TypedRelation<Attr: TypedAttribute>: Relation {
+    /// The underlying untyped `Relation` which provides our data.
     var wrapped: Relation
     
     public var scheme: Scheme {
@@ -30,6 +32,9 @@ public struct TypedRelation<Attr: TypedAttribute>: Relation {
 }
 
 public extension TypedRelation {
+    /// Do an async fetch of all values in this `Relation`, providing them in chunks as they become available.
+    /// Equivalent to `Relation.asyncBulkRows` but with a conversion step to this `TypeRelation`'s attribute
+    /// value type.
     func asyncBulkValues(_ callback: DispatchContextWrapped<(Result<Set<Attr.Value>, RelationError>) -> Void>) {
         var didError = false
         asyncBulkRows(callback.context.wrap({ result in
@@ -41,10 +46,15 @@ public extension TypedRelation {
         }))
     }
     
+    /// Do an async fetch of all values in this `Relation`, providing them in chunks as they become available.
+    /// Equivalent to `Relation.asyncBulkRows` but with a conversion step to this `TypeRelation`'s attribute
+    /// value type.
     func asyncBulkValues(_ callback: @escaping (Result<Set<Attr.Value>, RelationError>) -> Void) {
         asyncBulkValues(AsyncManager.currentInstance.runloopDispatchContext().wrap(callback))
     }
     
+    /// Do an async fetch of all values in this `Relation`, providing them all at once in a set. Equivalent to
+    /// `Relation.asyncBulkRows` but with a conversion step to this `TypeRelation`'s attribute value type.
     func asyncAllValues(_ callback: DispatchContextWrapped<(Result<Set<Attr.Value>, RelationError>) -> Void>) {
         asyncAllRows(callback.context.wrap({ result in
             let convertedResult = result.then(Attr.makeValues)
@@ -52,22 +62,28 @@ public extension TypedRelation {
         }))
     }
     
+    /// Do an async fetch of all values in this `Relation`, providing them all at once in a set. Equivalent to
+    /// `Relation.asyncBulkRows` but with a conversion step to this `TypeRelation`'s attribute value type.
     func asyncAllValues(_ callback: @escaping (Result<Set<Attr.Value>, RelationError>) -> Void) {
         asyncAllValues(AsyncManager.currentInstance.runloopDispatchContext().wrap(callback))
     }
 }
 
 public extension Relation {
+    /// Project this `Relation` onto a scheme of a single typed attribute, and return the projection as a
+    /// `TypedRelation` for that attribute.
     func project<Attr: TypedAttribute>() -> TypedRelation<Attr> {
         return TypedRelation(wrapped: self.project(Attr.name), debugName: nil)
     }
 }
 
 public extension TypedAttribute {
+    /// The specialized `TypedRelation` type you get from applying this typed attribute.
     public typealias Relation = TypedRelation<Self>
 }
 
 private extension TypedAttribute {
+    /// Transform a set of rows into a set of attribute values, or an error if any of the conversions fail.
     static func makeValues(_ rows: Set<Row>) -> Result<Set<Self.Value>, RelationError> {
         var set = Set<Self.Value>(minimumCapacity: rows.count)
         for row in rows {
