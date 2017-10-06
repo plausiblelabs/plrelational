5370013cc676dd44e974054e529bd20ebd88166b
Mike Ash <mikeash@plausible.coop>
2017-10-06 13:39:49 -0400

    Add some more docs to TypedAttribute.

--- PLRelational/TypedAttribute.swift
+++ PLRelational/TypedAttribute.swift
@@ -6,9 +6,14 @@
 import Foundation
 
 
+/// A type-safe version of an `Attribute`. By default, the attribute name is the conforming type's name.
+/// The conforming type specifies a value type which the `Attribute` is expected to hold.
 public protocol TypedAttribute {
+    /// The value type expected for this attribute.
     associatedtype Value: TypedAttributeValue
     
+    /// The underlying untyped `Attribute` for this typed attribute. By default, it's the name of the
+    /// conforming type with Swift's little "#1" extras stripped off.
     static var name: Attribute { get }
 }
 
@@ -24,37 +29,26 @@ public extension TypedAttribute {
     }
 }
 
+/// A type which can be used as the value for a typed attribute. These must be convertible
+/// to/from a RelationValue, and must also be Hashable so they can be stored in sets.
 public protocol TypedAttributeValue: Hashable {
+    /// Make a new instance of this value from a `RelationValue`, and return either
+    /// that value or an error.
     static func make(from: RelationValue) -> Result<Self, RelationError>
+    
+    /// Make a RelationValue that represents this value. Note: it is assumed that all
+    /// values can be so represented.
     var toRelationValue: RelationValue { get }
 }
 
+/// Errors that can be returned from `TypedAttributeValue.make`
 public enum TypedAttributeValueError: RelationError {
+    /// The `RelationValue` didn't contain the expected raw type (e.g. provided an int64 instead of text).
     case relationValueTypeMismatch
 }
 
-private extension RelationValue {
-    func typedValueOrError<T>(_ value: T?) -> Result<T, RelationError> {
-        return value.map(Result.Ok) ?? .Err(TypedAttributeValueError.relationValueTypeMismatch)
-    }
-    
-    func int64OrError() -> Result<Int64, RelationError> {
-        return typedValueOrError(get())
-    }
-    
-    func doubleOrError() -> Result<Double, RelationError> {
-        return typedValueOrError(get())
-    }
-    
-    func stringOrError() -> Result<String, RelationError> {
-        return typedValueOrError(get())
-    }
-    
-    func blobOrError() -> Result<[UInt8], RelationError> {
-        return typedValueOrError(get())
-    }
-}
-
+/// When conforming to `RelationValueConvertible`, we'll default to using the
+/// implementation of `relationValue` for converting to `RelationValue`.
 public extension TypedAttributeValue where Self: RelationValueConvertible {
     var toRelationValue: RelationValue {
         return self.relationValue
@@ -82,5 +76,28 @@ extension String: TypedAttributeValue {
 extension Data: TypedAttributeValue {
     public static func make(from: RelationValue) -> Result<Data, RelationError> {
         return from.blobOrError().map(Data.init)
+    }
+}
+
+/// Helpers for getting specific types out of a `RelationValue`.
+private extension RelationValue {
+    func typedValueOrError<T>(_ value: T?) -> Result<T, RelationError> {
+        return value.map(Result.Ok) ?? .Err(TypedAttributeValueError.relationValueTypeMismatch)
+    }
+    
+    func int64OrError() -> Result<Int64, RelationError> {
+        return typedValueOrError(get())
+    }
+    
+    func doubleOrError() -> Result<Double, RelationError> {
+        return typedValueOrError(get())
+    }
+    
+    func stringOrError() -> Result<String, RelationError> {
+        return typedValueOrError(get())
+    }
+    
+    func blobOrError() -> Result<[UInt8], RelationError> {
+        return typedValueOrError(get())
     }
 }
